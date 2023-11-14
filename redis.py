03e2c995ba0a3e7ea6844197af3d31eb62456a62
Robert <cruzr@moravian.edu>
2023-11-14 12:55:16 -0500

    redis updated

--- redis.py
+++ redis.py
@@ -1,20 +1,28 @@
+import csv
 import redis
-import os
+
+redis.client.StrictRedis(host='localhost', port=6379, db=0)
+
+file_path = 'colors.csv'
 
 
-def Redis():
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.flushall()
-    r.flushdb()
+def add_data_from_csv(file_path):
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            key = row['key']
+            value = row['value']
+            redis.set(key, value)
 
-    # open colors.csv
-    with open(os.path.join(os.path.dirname(__file__), 'colors.csv')) as c:
-        for line in c:
-            # split line into list
-            line = line.split(',')
-            # add color to redis
-            r.set(line[0], line[1].strip())
+            if 'list_value' in row:
+                list_key = key
+                list_values = row['list_value'].split(',')
+                # Push values to a list
+                redis.rpush(list_key, *list_values)
 
 
-if__name__ == '__main__':
-    Redis()
+if __name__ == "__main__":
+    csv_file_path = 'colors.csv'
+    add_data_from_csv(csv_file_path)
+    print('Data added successfully')
+    redis.close()
