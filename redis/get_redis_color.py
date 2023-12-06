8eb9fbc6a0ff64370dd3b78cc0861b7a711d14aa
Jeremy Goll <123649373+Jeremygoll@users.noreply.github.com>
2023-12-06 15:37:34 -0500

    Update get_redis_color.py

---
+++ redis/get_redis_color.py
@@ -0,0 +1,10 @@
+import redis
+from color_converter import clean_name
+
+
+def getColor(colorName):
+    r = redis.Redis(
+        host='localhost', port=6379, db=0)
+
+    value = r.hget("colors", clean_name(str(colorName)))
+    return value
